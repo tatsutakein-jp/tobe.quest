d298e3e788b5c3f288dd1b26163f9e1e9d006555
Ryo Takeuchi <develop@tatsutakein.jp>
2024-07-04 00:51:41 +0900

    chore: デプロイ関連に必要な処理を追加

--- plopfile.mjs
+++ plopfile.mjs
@@ -4,19 +4,40 @@
 export default function (plop) {
   plop.setGenerator('sample', {
     description: 'sample',
-    prompts: [{
-      type: 'input',
-      name: 'name',
-      message: 'sample name please'
-    }],
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'sample name please'
+      }
+    ],
     actions: [
       {
         type: 'addMany',
         templateFiles: 'templates/sample/**',
         destination: './sample/{{name}}',
         base: 'templates/sample',
-        abortOnFail: true,
-      },
+        abortOnFail: true
+      }
     ]
-  });
-};
+  })
+
+  plop.setGenerator('env', {
+    description: 'env',
+    prompts: [
+      {
+        type: 'input',
+        name: 'backendUrl',
+        message: 'backend url please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        templateFile: 'templates/env/.env.hbs',
+        path: '.env',
+        force: true
+      }
+    ]
+  })
+}
